d61183296dfd914b0bc42d0cdf46700eb816fc97
stephene369 <stephenew36@gmail.com>
2024-09-25 17:55:25 +0200

    Version-2024/09/25-17:55:25

---
+++ assets/images/main.py
@@ -0,0 +1,20 @@
+
+import cv2
+import numpy as np
+from rembg import remove
+from PIL import Image
+
+def remove_background(input_path, output_path):
+    # Read the image
+    input_image = Image.open(input_path)
+    
+    # Remove the background
+    output_image = remove(input_image)
+    
+    # Save the result
+    output_image.save(output_path)
+
+# Example usage
+input_path = 'me.jpg'
+output_path = 'output_image_no_bg.png'
+remove_background(input_path, output_path)
